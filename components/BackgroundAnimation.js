965b48bd407bffd13728c994aba8d905284a64d5
vikjuice <70850498+vikjuice@users.noreply.github.com>
2025-02-27 09:28:02 +0300

    Update BackgroundAnimation.js

--- components/BackgroundAnimation.js
+++ components/BackgroundAnimation.js
@@ -1,11 +1,1 @@
-import React from 'react';
 
-const BackgroundAnimation = () => {
-  return (
-    <div className="background-animation">
-      <div className="neon-spot"></div>
-    </div>
-  );
-};
-
-export default BackgroundAnimation;
