6eba98bf2b7e3f1123831b0aee4dce516ed78bbf
vikjuice <70850498+vikjuice@users.noreply.github.com>
2025-02-27 09:59:45 +0300

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = () => {
   return (
-    <a href="https://t.me/viktorkalyakin" target="_blank" rel="noopener noreferrer">
+    <a href="tg://resolve?domain=viktorkalyakin" target="_blank" rel="noopener noreferrer">
       <button className="contact-button">CONTACT</button>
     </a>
   );
